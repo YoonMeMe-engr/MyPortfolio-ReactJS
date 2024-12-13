da8621de1cec69b908ca7bc8c87d7742ef060042
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-12-13 12:16:29 +0630

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Components/Navbar/Navbar'
 import './App.css'
+import Intro from './Components/Intro/Intro'
 
 export default function App() {
     return (
       <div className='App'>
         <Navbar></Navbar>
+        <Intro></Intro>
       </div>
     )
 }
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Intro.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faSquareGithub} from '@fortawesome/free-brands-svg-icons'
+
+export default function Intro() {
+  return (
+    <div className='intro h-[77vh] mt-[6rem] mb-[10rem]'>
+        <div className="intro-left space-y-5">
+            <h1 className='text-5xl font-bold'>I am</h1>
+            <div className="text-5xl font-bold text-[#fca61f]">Ms. Yoon Me Me</div>
+            <div className="text-slate-500">Engineer with high level of experiences in IT, Telecom and Web Development, producting the Quality work</div>
+            <button className='button intro-btn'>Hire Me</button>
+            <div className="">
+                <FontAwesomeIcon icon={faSquareGithub} />
+            </div>
+        </div>
+        <div className="intro-right"></div>
+    </div>
+  )
+}
