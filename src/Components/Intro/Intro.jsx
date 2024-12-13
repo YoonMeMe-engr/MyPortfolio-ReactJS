230787fc493862eb2cbedaee30464a696fc3dc42
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-12-13 12:19:52 +0630

    first commit

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './Intro.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {faSquareGithub} from '@fortawesome/free-brands-svg-icons'
 
 export default function Intro() {
   return (
@@ -11,9 +9,9 @@ export default function Intro() {
             <div className="text-5xl font-bold text-[#fca61f]">Ms. Yoon Me Me</div>
             <div className="text-slate-500">Engineer with high level of experiences in IT, Telecom and Web Development, producting the Quality work</div>
             <button className='button intro-btn'>Hire Me</button>
-            <div className="">
+            {/* <div className="">
                 
-            </div>
+            </div> */}
         </div>
         <div className="intro-right"></div>
     </div>
