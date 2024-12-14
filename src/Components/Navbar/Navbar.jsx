0519648810218a6dbe4fe932fe525b571723e26c
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-12-14 18:24:47 +0630

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,13 @@
 import { Link } from 'react-scroll'
 import './Navbar.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import DarkMode from './DarkMode'
+import ResponsiveMenu from './ResponsiveMenu'
 
 export default function Navbar() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const menuOpenFunction = () => {
-        setMenuOpen(!menuOpen)
-    }
+    const [open, setOpen] = useState(false);
     return (
       <div className='flex justify-between items-center h-[10vh]' id="Navbar">
         <div className="nav-left flex items-center space-x-5">
@@ -41,31 +39,11 @@ export default function Navbar() {
                         Contact Me
                 </button>
             </Link>
-            <div className="lg:hidden">
-                <button onClick={menuOpenFunction} className='icon'>
-                    <FontAwesomeIcon icon={menuOpen ? faXmark : faBars} />
-                </button>
-                {menuOpen && (
-                    <ul className='flex flex-col fixed h-[15rem] w-[30rem] md:h-[30rem] md:w-[60rem] justify-center bg-white md:text-3xl text-xl space-y-5 md:space-y-10 left-52 md:left-96 md:bottom-[28rem] px-10 md:px-20 z-20'>
-                    <Link spy={true} to='Navbar' smooth={true} activeClass='active' className='hover:text-[#fca61f]'>
-                        <li>Home</li>
-                    </Link>
-                    <Link spy={true} to='Services' smooth={true} className='hover:text-[#fca61f]'>
-                        <li>Services</li>
-                    </Link>
-                    <Link spy={true} to='Experiences' smooth={true} className='hover:text-[#fca61f]'>
-                        <li>Experiences</li>
-                    </Link>
-                    <Link spy={true} to='Portfolio' smooth={true} className='hover:text-[#fca61f]'>
-                        <li>Portfolio</li>
-                    </Link>
-                    <Link spy={true} to='Testimonial' smooth={true} className='hover:text-[#fca61f]'>
-                        <li>Testimonials</li>
-                    </Link>                 
-                    </ul>
-                )}
-            </div>
+            <button className='mobile-menu lg:hidden' onClick={() => setOpen(!open)}>
+                <FontAwesomeIcon icon={faBars} className='text-4xl'/>
+            </button>
         </div>
+        <ResponsiveMenu open={open}></ResponsiveMenu>
       </div>
     )
 }
---
+++ src/Components/Navbar/ResponsiveMenu.jsx
@@ -0,0 +1,41 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import React from 'react'
+import { Link } from 'react-scroll'
+
+export default function ResponsiveMenu({open}) {
+  return (
+    <AnimatePresence mode='wait'>
+        {
+            open && (
+                <motion.div 
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+                exit={{ opacity: 0, y: -100 }}
+                className='absolute top-20 left-0 w-full h-screen z-20'
+                >
+                    <div className="text-xl uppercase bg-[lightblue] text-white py-10 m-6 rounded-3xl flex justify-center">
+                        <ul className='flex flex-col justify-center gap-10 cursor-pointer'>
+                            <Link spy={true} to='Navbar' smooth={true} activeClass='active' className='hover:text-[#fca61f]'>
+                                <li>Home</li>
+                            </Link>
+                            <Link spy={true} to='Services' smooth={true} className='hover:text-[#fca61f]'>
+                                <li>Services</li>
+                            </Link>
+                            <Link spy={true} to='Experiences' smooth={true} className='hover:text-[#fca61f]'>
+                                <li>Experiences</li>
+                            </Link>
+                            <Link spy={true} to='Portfolio' smooth={true} className='hover:text-[#fca61f]'>
+                                <li>Portfolio</li>
+                            </Link>
+                            <Link spy={true} to='Testimonial' smooth={true} className='hover:text-[#fca61f]'>
+                                <li>Testimonials</li>
+                            </Link>                 
+                        </ul>
+                    </div>
+                </motion.div>
+            )
+        }
+    </AnimatePresence>
+  )
+}
